4e6111b42b279845024b6c437c362d778371c934
mandar1n3 <blvackr0s3@gmail.com>
2026-05-21 01:01:21 +0200

    Use Inter as base font and remove old ones

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,11 @@ import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    fonts: [
-        {
-            provider: fontProviders.fontsource(),
-            name: "Playfair Display",
-            cssVariable: "--font-playfair-display",
-        },
-        {
-            provider: fontProviders.fontsource(),
-            name: "Fira Code",
-            cssVariable: "--font-fira-code",
-        }
-    ]
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Inter",
+      cssVariable: "--font-inter",
+    }
+  ]
 });
